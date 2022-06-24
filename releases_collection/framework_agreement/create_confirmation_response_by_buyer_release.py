ab0702264b06c7e562316ae9b09991c0fcdc02e8
roman <tatarenko.roman@gmail.com>
2022-06-24 19:49:32 +0300

    1. In progress refactor project: create branch 'version_4.0'

--- releases_collection/framework_agreement/create_confirmation_response_by_buyer_release.py
+++ releases_collection/framework_agreement/create_confirmation_response_by_buyer_release.py
@@ -1406,27 +1406,27 @@ class CreateConfirmationResponseRelease:
                                                         'releases'][0]['parties'][od]['persones'][act][
                                                         'businessFunctions'][act_1]['period']:
 
-                                                # # Set terminal id for 'persones[*].businessFucntions[*].id':
-                                                # try:
-                                                #     """Set permanent id."""
-                                                #     is_permanent_id_correct = is_it_uuid(
-                                                #         actual_fe_release['releases'][0]['parties'][od]['persones'][
-                                                #             act]['businessFunctions'][act_1]['id']
-                                                #     )
-                                                #     if is_permanent_id_correct is True:
-                                                #
-                                                #         actual_buyer_party['persones'][exp][
-                                                #             'businessFunctions'][exp_1]['id'] = \
-                                                #             actual_fe_release['releases'][0]['parties'][od][
-                                                #                 'persones'][act]['businessFunctions'][act_1]['id']
-                                                #     else:
-                                                #         actual_buyer_party['persones'][exp][
-                                                #             'businessFunctions'][exp_1]['id'] = \
-                                                #             f"'persones[{act}.businessFunctions[{act_1}].id' " \
-                                                #             f"must be UUID!"
-                                                # except KeyError:
-                                                #     KeyError(f"Mismatch key into path 'releases[0].parties[{od}]."
-                                                #              f"persones[{act}.businessFunctions[{act_1}].id'.")
+                                                # Set terminal id for 'persones[*].businessFucntions[*].id':
+                                                try:
+                                                    """Set permanent id."""
+                                                    is_permanent_id_correct = is_it_uuid(
+                                                        actual_fe_release['releases'][0]['parties'][od]['persones'][
+                                                            act]['businessFunctions'][act_1]['id']
+                                                    )
+                                                    if is_permanent_id_correct is True:
+
+                                                        actual_buyer_party['persones'][exp][
+                                                            'businessFunctions'][exp_1]['id'] = \
+                                                            actual_fe_release['releases'][0]['parties'][od][
+                                                                'persones'][act]['businessFunctions'][act_1]['id']
+                                                    else:
+                                                        actual_buyer_party['persones'][exp][
+                                                            'businessFunctions'][exp_1]['id'] = \
+                                                            f"'persones[{act}.businessFunctions[{act_1}].id' " \
+                                                            f"must be UUID!"
+                                                except KeyError:
+                                                    KeyError(f"Mismatch key into path 'releases[0].parties[{od}]."
+                                                             f"persones[{act}.businessFunctions[{act_1}].id'.")
 
                                                 if "documents" in actual_fe_release['releases'][0]['parties'][od][
                                                         'persones'][act]['businessFunctions'][act_1] and \
